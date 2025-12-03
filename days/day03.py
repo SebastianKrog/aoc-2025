480e41950f15963a319ef47ebee08103fde701d2
Sebastian Moretto Krog <sebastian.krog@gmail.com>
2025-12-03 07:35:05 +0100

    day3 part 1

---
+++ days/day03.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from typing import Any
+
+from re import findall
+#from itertools import combinations, permutations, product
+#from math import prod, lcm, ceil, floor, gcd
+
+#from aoc.progress import prog # Add a progress bar when needed (used as enumerate)
+
+#from aoc.grid import NORTH, SOUTH, EAST, WEST, DIR4, neighbors4, parse_char_grid,
+#  parse_int_grid, add_pos, UP, DOWN, LEFT, RIGHT, in_bounds
+#from aoc.grid import NORTH, SOUTH, EAST, WEST, DIR8, neighbors8, parse_char_grid,
+#  NORTH_EAST as NE, NORTH_WEST as NW, SOUTH_EAST as SE, SOUTH_WEST as SW
+#from aoc.search import bfs, bfs_one, dfs, astar, build_graph
+#from aoc.iteration import split_by, unique_permutations
+
+from aoc.common import read_input, time_call
+from aoc.config import AOC_YEAR
+
+DAY = 3
+
+
+def parse_input(raw: str) -> Any:
+    """Convert the raw text into a convenient structure."""
+    input = raw.rstrip("\n").splitlines()
+    return [findall(r"(\d)", l) for l in input]
+
+
+def part1(data: Any) -> Any:
+    """Solve part 1."""
+
+    def max_joltage(l):
+        for n in range(99,10,-1):
+            n1, n2 = list(str(n))
+            if n1 == n2:
+                if l.count(n1) > 1:
+                    return n
+            elif n1 in l:
+                n1i = l.index(n1)
+                if n2 in l[n1i:]:
+                    return n
+
+    return sum(max_joltage(l) for l in data)
+
+
+def part2(data: Any) -> Any:
+    """Solve part 2."""
+
+    return data
+
+
+def main() -> None:
+    raw = read_input(DAY)
+    data = parse_input(raw)
+
+    p1 = time_call(part1, data)
+    print(f"Year {AOC_YEAR} Day {DAY} - Part 1: {p1.value} ({p1.seconds:.3f}s)")
+
+    raw = read_input(DAY)
+    data = parse_input(raw)
+
+    #p2 = time_call(part2, data)
+    #print(f"Year {AOC_YEAR} Day {DAY} - Part 2: {p2.value} ({p2.seconds:.3f}s)")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_day03.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+import pytest
+
+from aoc.common import read_example
+
+DAY = 3
+
+
+def test_example_part1():
+    raw = read_example(DAY, idx=1)
+    from days.day03 import parse_input, part1
+
+    data = parse_input(raw)
+    # Replace 0 with the expected answer for part 1.
+    assert part1(data) == 357
+
+@pytest.mark.skip(reason="Skip until you've done part1")
+def test_example_part2():
+    raw = read_example(DAY, idx=1)
+    from days.day03 import parse_input, part2
+
+    data = parse_input(raw)
+    # Replace 0 with the expected answer for part 2.
+    assert part2(data) == 0
