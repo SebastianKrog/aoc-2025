cf227e82fe0120cb3db82a56c0e6c1edddf449e5
Sebastian Moretto Krog <sebastian.krog@gmail.com>
2025-12-06 09:57:50 +0100

    day6 part1+2

---
+++ days/day06.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from typing import Any
+
+import re
+#from itertools import combinations, permutations, product
+from math import prod #, lcm, ceil, floor, gcd
+
+#from aoc.progress import prog # Add a progress bar when needed (used as enumerate)
+
+#from aoc.grid import NORTH, SOUTH, EAST, WEST, DIR4, neighbors4, parse_char_grid,
+#  parse_int_grid, add_pos, UP, DOWN, LEFT, RIGHT, in_bounds
+#from aoc.grid import NORTH, SOUTH, EAST, WEST, DIR8, neighbors8, parse_char_grid,
+#  NORTH_EAST as NE, NORTH_WEST as NW, SOUTH_EAST as SE, SOUTH_WEST as SW
+#from aoc.search import bfs, bfs_one, dfs, astar, build_graph
+#from aoc.iteration import split_by, unique_permutations
+
+from aoc.common import read_input, time_call
+from aoc.config import AOC_YEAR
+
+DAY = 6
+
+
+def parse_input(raw: str) -> Any:
+    """Convert the raw text into a convenient structure."""
+    return raw.rstrip("\n").splitlines()
+    
+
+
+def part1(data: Any) -> Any:
+    """Solve part 1."""
+    data = [re.split(r"\s+", l.strip()) for l in data]
+    data = zip(*[data[-1]] + [[int(a) for a in l] for l in data[:-1]])
+
+    zum = 0
+    for terms in data:
+        if terms[0] == "*": zum += prod(terms[1:])
+        else: zum += sum(terms[1:])
+    return zum
+
+
+def part2(data: Any) -> Any:
+    """Solve part 2."""
+    data = zip(*[reversed(l) for l in data])
+
+    zum = 0
+    stack = []
+    for col in data:
+        if all(x == " " for x in col): continue
+        nums, symbol = col[:-1], col[-1]
+        stack.append(int("".join(nums)))
+        if symbol == "+": zum += sum(stack)
+        if symbol == "*": zum += prod(stack)
+        if symbol != " ": stack = []
+            
+    return zum
+
+
+def main() -> None:
+    raw = read_input(DAY)
+    data = parse_input(raw)
+
+    p1 = time_call(part1, data)
+    print(f"Year {AOC_YEAR} Day {DAY} - Part 1: {p1.value} ({p1.seconds:.3f}s)")
+
+    raw = read_input(DAY)
+    data = parse_input(raw)
+
+    p2 = time_call(part2, data)
+    print(f"Year {AOC_YEAR} Day {DAY} - Part 2: {p2.value} ({p2.seconds:.3f}s)")
+
+
+if __name__ == "__main__":
+    main()
