40eac23fb877831d48b2f5caa38feab26ba14c6b
Sebastian Moretto Krog <sebastian.krog@gmail.com>
2025-12-11 13:43:21 +0100

    day10 part 2 not quite there yet

--- days/day10.py
+++ days/day10.py
@@ -12,7 +12,7 @@ from aoc.progress import prog # Add a progress bar when needed (used as enumerat
 #  parse_int_grid, add_pos, UP, DOWN, LEFT, RIGHT, in_bounds
 #from aoc.grid import NORTH, SOUTH, EAST, WEST, DIR8, neighbors8, parse_char_grid,
 #  NORTH_EAST as NE, NORTH_WEST as NW, SOUTH_EAST as SE, SOUTH_WEST as SW
-from aoc.search import bfs_one #, dfs, astar, build_graph, bfs
+from aoc.search import bfs_one, dijkstra_one
 #from aoc.iteration import split_by, unique_permutations, nwise
 
 from aoc.common import read_input, time_call
@@ -74,33 +74,72 @@ def part2(data: Any) -> Any:
     """Solve part 2."""
 
     def fewest_presses(bts, joltages):
-        # For each joltage dial, find the buttons that can manipulate it
-        j_bts = {i: set() for i,_ in enumerate(joltages)}
-        for i, dial in enumerate(joltages):
-            for j, btn in enumerate(bts):
-                if i in btn: pass
+        value = sum(joltages)
+        print(value)
 
-        # For each btn, find the max number of times it can be pressed
+        # For each joltage dial, find the buttons that can manipulate it
+        j_bts = {i: set() for i, _ in enumerate(joltages)}
+
+        # bts_max: For each btn, find the max number of times it can be pressed
+        # bts_val: For each btn, the number of joltages it will increase (len)
         bts_max = {}
+        bts_val = {}
         for i, btn in enumerate(bts):
             btn_max = 10**6
+            bts_val[i] = len(btn)
             for j, dial in enumerate(btn):
-               j_bts[i].add(j)
+               j_bts[dial].add(i)
                btn_max = min(joltages[dial], btn_max)
             bts_max[i] = btn_max
 
-        # Now, we know that number of btn presses must equal the dial settings
         print(j_bts, bts_max)
+
+        # Possible minor optimization:
+        for dial, bts_i in j_bts.items():
+            if len(bts_i) == 1: pass # We know that only one button can set this dial
+
+        # Now, we know that number of btn presses must equal the dial settings
+        #
+        # for btn a->d and dial x,y,z,q
+        # x = a + b
+        # y = b + c + d
+        # z = a + d
+        # q = b + d
+        # min(a+b+c+d)
+
+        # a = x - b, a = z - d => b = -z + d + x
+        # b = y - c - d, b = q - d =>  -z + d + x = y - c - d,  -z + d + x = q - d
+        # d = a - z
+
+        # Let's try dijkstra. The edge cost will be max(bts_val.values() - bts_val(i))
+        # So we greedily push the button that increments the most buttons first
+        # But we don't care about order... so x y x y == y x y x (order of pushes)
+
+        start = (0,) * len(bts)
+
+        def check_press(node, goal = False):
+            for dial in j_bts:
+                dial_val = sum(node[i] for i in dial)
+                if dial_val > joltages[dial]: return False
+                if goal and dial_val < joltages[dial]: return False
+            return True
+
+        def neighbor(node):
+            for i, btn in enumerate(bts):
+                if bts_max[i] < node[i] + 1: continue
+                nxt = node[:i] + (node[i] + 1,) + node[i+1:], -bts_val[i]
+                if sum(bts_val[i]*j for i, j in range(btn)) > value: continue
+                if check_press(nxt):
+                    yield tuple(nxt), -bts_val[i]
+        
+        return dijkstra_one(start, neighbor, lambda x: check_press(x, goal=True)).goal_cost()
+
     
     for _,b,j in data:
-        print(list(b),list(j))
-        fewest_presses(b,j)
+        buttons = tuple(list(a) for  a in b)
+        fewest_presses(tuple(buttons),tuple(j))
                 
     return None
-    
-
-
-    return data
 
 
 def main() -> None:
