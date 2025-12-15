c74a264ae1e1e8b083c45a0bd6ceb51ad47d28d7
Sebastian Moretto Krog <sebastian.krog@gmail.com>
2025-12-15 16:11:08 +0100

    Update test_day01.py

--- tests/test_day01.py
+++ tests/test_day01.py
@@ -17,5 +17,4 @@ def test_example_part2():
     raw = read_example(DAY, idx=1)
     from days.day01 import parse_input, part2
 
-    # Replace 0 with the expected answer for part 2.
     assert part2(parse_input(raw)) == 6
